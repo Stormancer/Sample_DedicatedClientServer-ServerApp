2daa0462ebc8bd62f417e5cc8e4f374c2843d2cc
Vincs <desfontaines.vincent@gmail.com>
2017-07-13 18:58:17 +0200

    sqdqds

--- Plugins/GameSession/GameSessionService.cs
+++ Plugins/GameSession/GameSessionService.cs
@@ -413,6 +413,7 @@ namespace Server.Plugins.GameSession
                 prc.StartInfo.EnvironmentVariables.Add("connectionToken", token);
                 _logger.Log(LogLevel.Debug, "gameserver", $"Starting server {prc.StartInfo.FileName} with args {prc.StartInfo.Arguments}", new { env = prc.StartInfo.EnvironmentVariables });
                 
+                
                 //prc.OutputDataReceived += (sender, args) =>
                 //{
                 //    if (verbose)
